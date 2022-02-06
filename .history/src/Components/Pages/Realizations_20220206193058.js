b73bd4d85fb1fc465405cd3e2cf2a72090ae4e9a
MacBook <macbook@MacBook-Pro-MacBook.local>
2022-02-06 19:32:04 +0100

    Add new photos

---
+++ .history/src/Components/Pages/Realizations_20220206193058.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "../Styles/Realizations.css";
+import Zoom from "react-reveal/Zoom";
+
+function Realizations() {
+  return (
+    <div className="realizations">
+      <h2 style={{ color: "#434343" }}>Portfolio</h2>
+
+      <div className="realizations-container">
+        <Zoom>
+        <img src="/images/2022/portfolio/g1.jpg" alt="" />
+        <img src="/images/2022/portfolio/g2.jpg" alt="" />
+        <img src="/images/2022/portfolio/g3.jpg" alt="" />
+        <img src="/images/2022/portfolio/g4.jpg" alt="" />
+        <img src="/images/2022/portfolio/g5.jpg" alt="" />
+        <img src="/images/2022/portfolio/g6.jpg" alt="" />
+        <img src="/images/2022/portfolio/g7.jpg" alt="" />
+        <img src="/images/2022/portfolio/g8.jpg" alt="" />
+        <img src="/images/2022/portfolio/g9.jpg" alt="" />
+        <img src="/images/2022/portfolio/g10.jpg" alt="" />
+        <img src="/images/2022/portfolio/g11.jpg" alt="" />
+        <img src="/images/2022/portfolio/g12.jpg" alt="" />
+        <img src="/images/2022/portfolio/g13.jpg" alt="" />
+        <img src="/images/2022/portfolio/g14.jpg" alt="" />
+        <img src="/images/2022/portfolio/g15.jpg" alt="" />
+        <img src="/images/2022/portfolio/g16.jpg" alt="" />
+        <img src="/images/2022/portfolio/g17.jpg" alt="" />
+        <img src="/images/2022/portfolio/g18.jpg" alt="" />
+        <img src="/images/2022/portfolio/g19.jpg" alt="" />
+        <img src="/images/2022/portfolio/g20.jpg" alt="" />
+        <img src="/images/2022/portfolio/g21.jpg" alt="" />
+        <img src="/images/2022/portfolio/g22.jpg" alt="" />
+        <img src="/images/2022/portfolio/g23.jpg" alt="" />
+        <img src="/images/2022/portfolio/g24.jpg" alt="" />
+        <img src="/images/2022/portfolio/g25.jpg" alt="" />
+        <img src="/images/2022/portfolio/g26.jpg" alt="" />
+        <img src="/images/2022/portfolio/g27.jpg" alt="" />
+        <img src="/images/2022/pportfolio/g28.jpg" alt="" />
+        </Zoom>
+      </div>
+    </div>
+  );
+}
+
+export default Realizations;
